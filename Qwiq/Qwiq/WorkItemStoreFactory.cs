a76cac95135545cbfec47bb7a1148a4c6a8f096a
Matt Kotsenas <mattkot@microsoft.com>
2015-03-06 09:43:13 -0800

    Create a factory for WorkItemStore
    Make WorkItemStoreProxy's constructor internal and create a factory for
    creating work item stores.

---
+++ Qwiq/Qwiq/WorkItemStoreFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.IE.Qwiq.Credentials;
+
+namespace Microsoft.IE.Qwiq
+{
+    public interface IWorkItemStoreFactory
+    {
+        IWorkItemStore Create(Uri endpoint, TfsCredentials credentials);
+    }
+
+    public class WorkItemStoreFactory : IWorkItemStoreFactory
+    {
+        private WorkItemStoreFactory()
+        {
+        }
+
+        public static IWorkItemStoreFactory GetInstance()
+        {
+            return new WorkItemStoreFactory();
+        }
+
+        public IWorkItemStore Create(Uri endpoint, TfsCredentials credentials)
+        {
+            return new WorkItemStoreProxy(endpoint, credentials);
+        }
+    }
+}
