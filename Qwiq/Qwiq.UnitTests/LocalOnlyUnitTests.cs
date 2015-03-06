a8963d6e49a7d3189085c0a466961058cd8a5392
pelavall@microsoft.com <pelavall@microsoft.com>
2015-03-05 23:45:08 -0800

    Adding localonly unit test for LinkCollection contain and removal

---
+++ Qwiq/Qwiq.UnitTests/LocalOnlyUnitTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Microsoft.IE.IEPortal.BehaviorDrivenDevelopmentTools;
+using Microsoft.IE.Qwiq.Credentials;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.IE.Qwiq.UnitTests
+{
+    public class LocalOnlyTests : ContextSpecification
+    {
+        private const string VisualStudioResourceString = "499b84ac-1321-427f-aa17-267ca6975798";
+        private const string TFSClientId = "872cd9fa-d31f-45e0-9eab-6e460a02d1f1";
+        private const string Authority = "https://login.windows.net/common/";
+        protected IWorkItemStore WorkItemStore { get; set; }
+
+        public override void Given()
+        {
+            var credentials = CredentialsFactory.GetInstance().CreateAadCredentials(VisualStudioResourceString, TFSClientId, Authority);
+            var connectionUri = new Uri("https://microsoft.visualstudio.com/DefaultCollection");
+            WorkItemStore = new WorkItemStoreProxy(connectionUri, credentials);
+        }
+    }
+
+    [TestClass]
+    public class given_a_workitemstore_when_a_link_is_removed : LocalOnlyTests
+    {
+        private bool Result { get; set; }
+        private IWorkItem TargetWorkItem { get; set; }
+        private ILink TargetLink { get; set; }
+
+        public override void Given()
+        {
+            base.Given();
+            IWorkItem wi = WorkItemStore.Query("SELECT * FROM WorkItems WHERE [System.ID] IN (1542232)").Single();
+            TargetWorkItem = WorkItemStore.Query("SELECT * FROM WorkItems WHERE [System.ID] IN (1523767)").Single();
+            TargetLink = TargetWorkItem.CreateRelatedLink(WorkItemLinkDirection.Forward, wi);
+        }
+
+        public override void When()
+        {
+            Result = TargetWorkItem.Links.Remove(TargetLink);
+        }
+
+        [TestCategory("localOnly")]
+        [TestMethod]
+        public void the_result_of_the_removal_is_true()
+        {
+            Result.ShouldBeTrue();
+        }
+
+        [TestCategory("localOnly")]
+        [TestMethod]
+        public void the_link_is_actually_removed()
+        {
+            TargetWorkItem.Links.Contains(TargetLink).ShouldBeFalse();
+        }
+    }
+}
