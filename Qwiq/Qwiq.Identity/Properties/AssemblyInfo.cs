6dae134a34d8285404b2cdeeb93bb34d01de776d
pelavall@microsoft.com <pelavall@microsoft.com>
2015-03-06 17:28:16 -0800

    Adding nuget artifacts for Qwiq.Identity

--- Qwiq/Qwiq.Identity/Properties/AssemblyInfo.cs
+++ Qwiq/Qwiq.Identity/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Qwiq.Identity")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Microsoft.IE.Qwiq.Identity")]
+[assembly: AssemblyDescription("Provides Microsoft Identity extension methods on top of Microsoft.IE.Qwiq.Core")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Qwiq.Identity")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("ieportal@microsoft.com Cantaloop@microsoft.com")]
+[assembly: AssemblyProduct("Microsoft.IE.Qwiq.Identity")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
