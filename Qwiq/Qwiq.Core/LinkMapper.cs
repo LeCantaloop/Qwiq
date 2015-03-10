79cdd875ba4c65c2d8c77d43d97611707461ed8f
Matt Kotsenas <mattkot@microsoft.com>
2015-03-10 11:32:11 -0700

    Extract LinkMapper and add unit tests
    Extract the LinkMapper methods from the previous changes into its own
    class and add Fakes tests to verify that mapping is happening correctly.

---
+++ Qwiq/Qwiq.Core/LinkMapper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.IE.Qwiq.Proxies;
+using Tfs = Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace Microsoft.IE.Qwiq
+{
+    internal class LinkMapper
+    {
+        public Tfs.Link Map(ILink link, Tfs.WorkItem item)
+        {
+            var relatedLink = link as IRelatedLink;
+            if (relatedLink != null)
+            {
+                var linkTypeEnd = LinkDirectionMapper.Map(item.Store, relatedLink.LinkDirection);
+                return new Tfs.RelatedLink(linkTypeEnd, relatedLink.RelatedWorkItemId);
+            }
+
+            var hyperlink = link as IHyperlink;
+            if (hyperlink != null)
+            {
+                return new Tfs.Hyperlink(hyperlink.Location);
+            }
+
+            var workItemLink = link as IWorkItemLink;
+            if (workItemLink != null)
+            {
+                var linkTypeEnd = LinkDirectionMapper.Map(item.Store, workItemLink.LinkDirection);
+                return new Tfs.WorkItemLink(linkTypeEnd, workItemLink.SourceId, workItemLink.TargetId);
+            }
+
+            throw new ArgumentException("Unknown link type", "link");
+        }
+
+        public ILink Map(Tfs.Link link)
+        {
+            var relatedLink = link as Tfs.RelatedLink;
+            if (relatedLink != null)
+            {
+                return new RelatedLinkProxy(relatedLink);
+            }
+
+            var hyperlink = link as Tfs.Hyperlink;
+            if (hyperlink != null)
+            {
+                return new HyperlinkProxy(hyperlink);
+            }
+
+            var workItemLink = link as Tfs.WorkItemLink;
+            if (workItemLink != null)
+            {
+                return new WorkItemLinkProxy(workItemLink);
+            }
+
+            throw new ArgumentException("Unknown link type", "link");
+        }
+    }
+}
---
+++ Qwiq/Qwiq.UnitTests/LinkMapperTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.IE.IEPortal.BehaviorDrivenDevelopmentTools;
+using Microsoft.IE.Qwiq.Proxies;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tfs = Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace Microsoft.IE.Qwiq.UnitTests
+{
+    public abstract class LinkMapperTests : ContextSpecification
+    {
+        internal LinkMapper Mapper;
+        protected Tfs.WorkItem WorkItem;
+        protected Tfs.WorkItemStore WorkItemStore;
+        private IDisposable _shimsContext;
+
+        public override void Given()
+        {
+            _shimsContext = ShimsContext.Create();
+
+            var end = new Tfs.Fakes.ShimWorkItemLinkTypeEnd();
+
+            var workItemLinkType = new Tfs.Fakes.ShimWorkItemLinkType
+            {
+                ReferenceNameGet = () => "System.LinkTypes.Hierarchy",
+                ForwardEndGet = () => end,
+                ReverseEndGet = () => end
+            };
+
+            var workItemLinkTypeCollection = new Tfs.Fakes.ShimWorkItemLinkTypeCollection
+            {
+                SystemCollectionsIEnumerableGetEnumerator = () => new[] {workItemLinkType}.GetEnumerator()
+            };
+
+            WorkItemStore = new Tfs.Fakes.ShimWorkItemStore
+            {
+                WorkItemLinkTypesGet = () => workItemLinkTypeCollection
+            };
+
+            WorkItem = new Tfs.Fakes.ShimWorkItem
+            {
+                StoreGet = () => WorkItemStore
+            };
+
+            Mapper = new LinkMapper();
+        }
+
+        public override void Cleanup()
+        {
+            _shimsContext.Dispose();
+        }
+    }
+
+    [TestClass]
+    public class given_a_tfs_hyperlink_and_a_LinkMapper_when_the_hyperlink_is_mapped : LinkMapperTests
+    {
+        private Tfs.Link _tfsLink;
+        private IHyperlink _actual;
+        private const string Domain = "http://domain.tld";
+
+        public override void Given()
+        {
+            base.Given();
+            _tfsLink = new Tfs.Hyperlink(Domain);
+        }
+
+        public override void When()
+        {
+            _actual = Mapper.Map(_tfsLink) as IHyperlink;
+        }
+
+        [TestMethod]
+        public void the_LinkMapper_returns_a_corresponding_IHyperlink()
+        {
+            _actual.Location.ShouldEqual(Domain);
+        }
+    }
+
+    [TestClass]
+    public class given_an_ILink_and_a_LinkMapper_when_the_ILink_is_mapped : LinkMapperTests
+    {
+        private Tfs.Hyperlink _expected;
+        private ILink _link;
+        private Tfs.Hyperlink _actual;
+
+        public override void Given()
+        {
+            base.Given();
+            _expected = new Tfs.Hyperlink("http://domain.tld");
+            _link = new HyperlinkProxy(_expected);
+        }
+
+        public override void When()
+        {
+            _actual = Mapper.Map(_link, WorkItem) as Tfs.Hyperlink;
+        }
+
+        [TestMethod]
+        public void the_LinkMapper_returns_a_new_tfs_Link_with_the_same_fields_as_the_original_tfs_Link()
+        {
+            _actual.Location.ShouldEqual(_expected.Location);
+        }
+    }
+}
