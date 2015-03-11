d18b44196124e4b899d12f82b562178085de7f99
Matt Kotsenas <mattkot@microsoft.com>
2015-03-11 16:38:03 -0700

    Expose ValidationException from Query

---
+++ Qwiq/Qwiq.Core/ValidationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Tfs = Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace Microsoft.IE.Qwiq
+{
+    public class ValidationException : Exception
+    {
+        internal ValidationException(Tfs.ValidationException ex) : base(ex.Message)
+        {
+        }
+    }
+}
