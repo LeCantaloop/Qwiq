2e805b154fd51f4f38dcfb89019382da1f05963f
Matt Kotsenas <mattkot@microsoft.com>
2015-03-17 09:55:39 -0700

    Correct typo in IdentityDescriptorProxy
    Typo in IdentityDescriptorProxy was causing the same field to be returned
    twice.

---
+++ Qwiq/Qwiq.UnitTests/IdentityDescriptorProxyTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IE.IEPortal.BehaviorDrivenDevelopmentTools;
+using Microsoft.IE.Qwiq.Proxies;
+using Microsoft.TeamFoundation.Framework.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.IE.Qwiq.UnitTests
+{
+    public abstract class IdentityDescriptorProxyTests : ContextSpecification
+    {
+        protected IdentityDescriptorProxy IdentityDescriptorProxy;
+        protected IdentityDescriptor IdentityDescriptor;
+
+        public override void When()
+        {
+            IdentityDescriptorProxy = new IdentityDescriptorProxy(IdentityDescriptor);
+        }
+    }
+
+    [TestClass]
+    public class given_an_IdentityDescriptor_when_a_proxy_is_created : IdentityDescriptorProxyTests
+    {
+        private const string Identifier = "identifier";
+        private const string IdentityType = "identityType";
+
+        public override void Given()
+        {
+            IdentityDescriptor = new IdentityDescriptor(IdentityType, Identifier);
+        }
+
+        [TestMethod]
+        public void the_identifier_can_be_retrieved()
+        {
+            IdentityDescriptorProxy.Identifier.ShouldEqual(Identifier);
+        }
+
+        [TestMethod]
+        public void the_identity_type_can_be_retrieve()
+        {
+            IdentityDescriptorProxy.IdentityType.ShouldEqual(IdentityType);
+        }
+    }
+}
