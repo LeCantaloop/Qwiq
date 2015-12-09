3ffe309325e67a98ddc5be8b2f1bc1dd95e0e309
Peter Lavallee <pelavall@microsoft.com>
2015-12-08 21:29:47 -0800

    Updating existing Qwiq Exceptions to preserve inner exception

--- Qwiq/Qwiq.Core/ItemAlreadyUpdatedOnServerException.cs
+++ Qwiq/Qwiq.Core/ItemAlreadyUpdatedOnServerException.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.IE.Qwiq.Exceptions;
 using Microsoft.IE.Qwiq.Proxies;
 using Tfs = Microsoft.TeamFoundation.WorkItemTracking.Client;
 
@@ -10,14 +11,14 @@ namespace Microsoft.IE.Qwiq
     {
         private readonly Tfs.ItemAlreadyUpdatedOnServerException _exception;
 
-        internal ItemAlreadyUpdatedOnServerException(Tfs.ItemAlreadyUpdatedOnServerException exception)
+        internal ItemAlreadyUpdatedOnServerException(Tfs.ItemAlreadyUpdatedOnServerException exception) : base(exception.Message, exception)
         {
             _exception = exception;
         }
 
         public IEnumerable<IFieldConflict> FieldConflicts
         {
-            get { return _exception.FieldConflicts.Select(item => new FieldConflictProxy(item)); }
+            get { return _exception.FieldConflicts.Select(item => ExceptionHandlingDynamicProxyFactory.Create<IFieldConflict>(new FieldConflictProxy(item))); }
         }
     }
 }
