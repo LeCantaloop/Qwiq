f1221e32c274dda691fb88cbddfcdb1990c3a4b2
pelavall <pelavall@microsoft.com>
2015-12-09 10:54:50 -0800

    Fix bug where AggregateExceptions were not flattened.
    	- Added test for nested AggregateExceptions

--- Qwiq/Qwiq.Core.Tests/AggregateExceptionExploderTests.cs
+++ Qwiq/Qwiq.Core.Tests/AggregateExceptionExploderTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.IE.IEPortal.BehaviorDrivenDevelopmentTools;
 using Microsoft.IE.Qwiq.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -37,6 +38,30 @@ namespace Microsoft.IE.Qwiq.Core.Tests
         public void the_result_is_an_empty_enumerable()
         {
             Result.ShouldBeEmpty();
+        }
+    }
+
+    [TestClass]
+    public class given_an_AggregateException_containing_AggregateExceptions_when_exploded : AggregateExceptionExploderTests
+    {
+        private IEnumerable<Exception> ExpectedExceptions { get; set; }
+
+        public override void Given()
+        {
+            base.Given();
+
+            var exception1 = new ArgumentException();
+            var exception2 = new ArgumentException();
+
+            ExpectedExceptions = new[] {exception1, exception2};
+
+            Exception = new AggregateException(exception1, new AggregateException(exception2, new AggregateException()));
+        }
+
+        [TestMethod]
+        public void the_result_should_have_the_expected_exceptions()
+        {
+            Result.ShouldContainOnly(ExpectedExceptions);
         }
     }
 }
