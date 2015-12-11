4c6817dd447d8485ca9e14de1df747a124559bc2
pelavall <pelavall@microsoft.com>
2015-12-11 14:43:53 -0800

    Update addtional Qwiq exceptions to inherit from InvalidOperationException
    	-ItemAlreadyUpdatedOnServer and ServerRejectedChanges

--- Qwiq/Qwiq.Core/ServerRejectedChangesException.cs
+++ Qwiq/Qwiq.Core/ServerRejectedChangesException.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Microsoft.IE.Qwiq
 {
-    public class ServerRejectedChangesException : Exception
+    public class ServerRejectedChangesException : InvalidOperationException
     {
         internal ServerRejectedChangesException(Exception ex) : base(ex.Message, ex)
         {
