35b6b2d9ffdaed394d288991773a50af99d185a9
Richard Murillo <richard.murillo@microsoft.com>
2016-02-25 10:13:18 -0800

    Add support for GitVersion
    GitVersion is a tool to help you achieve Semantic Versioning on your project.
    
    http://gitversion.readthedocs.org/en/latest/

--- Qwiq/Qwiq.Mapper/Properties/AssemblyInfo.cs
+++ Qwiq/Qwiq.Mapper/Properties/AssemblyInfo.cs
@@ -20,17 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6ee68fb3-44b3-40e5-ae64-5630cf246948")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
