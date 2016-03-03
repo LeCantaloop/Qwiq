3487648d59e46384f2f5fbd79722cd565fd30ae2
Peter Lavallee <pelavall@microsoft.com>
2016-03-03 15:37:38 -0800

    Add FieldMapper classes to Qwiq.Linq

---
+++ Qwiq/Qwiq.Linq/CachingFieldMapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Microsoft.IE.Qwiq.Linq
+{
+    public class CachingFieldMapper : IFieldMapper
+    {
+        private readonly IFieldMapper _innerMapper;
+        private readonly ConcurrentDictionary<string, object> _cache;
+
+        public CachingFieldMapper(IFieldMapper innerMapper)
+        {
+            _innerMapper = innerMapper;
+            _cache = new ConcurrentDictionary<string, object>();
+        }
+
+        public string GetWorkItemType(Type type)
+        {
+            return GetOrAdd(GenerateCacheKey(type, "GetWorkItemType"), () => _innerMapper.GetWorkItemType(type));
+        }
+
+        public IEnumerable<string> GetFieldNames(Type type)
+        {
+            return GetOrAdd(GenerateCacheKey(type, "GetFieldNames"), () => _innerMapper.GetFieldNames(type));
+        }
+
+        public string GetFieldName(Type type, string propertyName)
+        {
+            return GetOrAdd(GenerateCacheKey(type, "GetFieldName", propertyName),
+                () => _innerMapper.GetFieldName(type, propertyName));
+        }
+
+        private T GetOrAdd<T>(string key, Func<T> func)
+        {
+            return (T)_cache.GetOrAdd(key, val => func());
+        }
+
+        private string GenerateCacheKey(Type type, string method, string propertyName = "")
+        {
+            return type.AssemblyQualifiedName + method + propertyName;
+        }
+    }
+}
