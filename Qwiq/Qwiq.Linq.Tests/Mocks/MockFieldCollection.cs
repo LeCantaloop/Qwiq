6104ccef091a28b0e9321dbfc2f9a2c78634a47d
Peter Lavallee <pelavall@microsoft.com>
2016-04-14 22:48:01 -0700

    Update Qwiq.Linq to latest version of Qwiq.Mapper (#82)

---
+++ Qwiq/Qwiq.Linq.Tests/Mocks/MockFieldCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.IE.Qwiq.Linq.Tests.Mocks
+{
+    public class MockFieldCollection : IFieldCollection
+    {
+        private readonly ICollection<IField> _mockFields;
+
+        public MockFieldCollection(IEnumerable<IField> mockFields)
+        {
+            _mockFields = mockFields.ToList();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _mockFields.GetEnumerator();
+        }
+
+        public bool Contains(string fieldName)
+        {
+            return _mockFields.Any(f => f.Name.Equals(fieldName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public IField this[string name]
+        {
+            get { return _mockFields.First(f => f.Name.Equals(name, StringComparison.OrdinalIgnoreCase)); }
+        }
+
+        public int Count
+        {
+            get { return _mockFields.Count; }
+        }
+    }
+}
