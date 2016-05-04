acce530697aeeb4a8a8db4576ab2e66465fdb2bb
Peter Lavallee <pelavall@microsoft.com>
2016-05-03 17:19:27 -0700

    Move identity mapping logic in IdentityVisitor to own class

---
+++ Qwiq/Qwiq.Identity/Linq/Visitors/IdentityMapper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.IE.Qwiq.Identity.Linq.Visitors
+{
+    public class IdentityMapper : IIdentityMapper
+    {
+        private readonly IIdentityManagementService _identityManagementService;
+        private readonly string _tenantId;
+        private readonly string[] _domains;
+
+        public IdentityMapper(IIdentityManagementService identityManagementService, string tenantId, params string[] domains)
+        {
+            _identityManagementService = identityManagementService;
+            _tenantId = tenantId;
+            _domains = domains;
+        }
+
+        private string GetDisplayName(string alias)
+        {
+            var identity = _identityManagementService.GetIdentityForAlias(alias, _tenantId, _domains);
+            return identity?.DisplayName ?? alias;
+        }
+
+        public object Map(object value)
+        {
+            var stringValue = value as string;
+            if (stringValue != null)
+            {
+                return GetDisplayName(stringValue);
+            }
+
+            var stringArray = value as IEnumerable<string>;
+            if (stringArray != null)
+            {
+                return stringArray.Select(GetDisplayName);
+            }
+
+            return value;
+        }
+    }
+}
