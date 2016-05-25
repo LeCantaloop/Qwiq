360cf7881f892f22cf91316d5e8fc8527b872716
Richard Murillo <rimuri@microsoft.com>
2016-05-25 16:51:30 -0700

    Migrate Qwiq mocks to common project mocks

--- Qwiq/Qwiq.Linq.Tests/QueryTestsBase.cs
+++ Qwiq/Qwiq.Linq.Tests/QueryTestsBase.cs
@@ -5,6 +5,7 @@ using Microsoft.IE.Qwiq.Linq.Tests.Mocks;
 using Microsoft.IE.Qwiq.Linq.Visitors;
 using Microsoft.IE.Qwiq.Mapper;
 using Microsoft.IE.Qwiq.Mapper.Attributes;
+using Microsoft.IE.Qwiq.Mocks;
 
 namespace Microsoft.IE.Qwiq.Linq.Tests
 {
@@ -21,99 +22,43 @@ namespace Microsoft.IE.Qwiq.Linq.Tests
         {
             var workItems = new List<IWorkItem>
             {
-                new MockWorkItem
-                {
-                    Id = 1,
-                    Type = new MockWorkItemType
-                    {
-                        Name = "SimpleMockWorkItem"
-                    },
-                    Properties = new Dictionary<string, object>
+                new MockWorkItem("SimpleMockWorkItem", new Dictionary<string, object>
                     {
                         {"ID", 1},
                         {"IntField", 2}
-                    }
-                },
-                new MockWorkItem
-                {
-                    Id = 2,
-                    Type = new MockWorkItemType
-                    {
-                        Name = "SimpleMockWorkItem"
-                    },
-                    Properties = new Dictionary<string, object>
+                    }),
+                new MockWorkItem("SimpleMockWorkItem", new Dictionary<string, object>
                     {
                         {"ID", 2},
                         {"IntField", 4}
-                    }
-                },
-                new MockWorkItem
-                {
-                    Id = 3,
-                    Type = new MockWorkItemType
-                    {
-                        Name = "SimpleMockWorkItem"
-                    },
-                    Properties = new Dictionary<string, object>
+                    })
+,
+                new MockWorkItem("SimpleMockWorkItem", new Dictionary<string, object>
                     {
                         {"ID", 3},
                         {"IntField", 3}
-                    }
-                },
-                new MockWorkItem
-                {
-                    Id = 4,
-                    Type = new MockWorkItemType
-                    {
-                        Name = "SimpleMockWorkItem"
-                    },
-                    Properties = new Dictionary<string, object>
+                    })
+,
+                new MockWorkItem("SimpleMockWorkItem", new Dictionary<string, object>
                     {
                         {"ID", 4},
                         {"IntField", 4}
-                    }
-                },
-                new MockWorkItem
-                {
-                    Id = 5,
-                    Type = new MockWorkItemType
-                    {
-                        Name = "SimpleMockWorkItem"
-                    },
-                    Properties = new Dictionary<string, object>
+                    })
+,
+                new MockWorkItem("SimpleMockWorkItem", new Dictionary<string, object>
                     {
                         {"ID", 5},
                         {"IntField", 5}
-                    }
-                }
+                    })
+
             };
 
             var links = new[] {
-                new MockWorkItemLinkInfo
-                {
-                    SourceId = 0,
-                    TargetId = 3
-                },
-                new MockWorkItemLinkInfo
-                {
-                    SourceId = 3,
-                    TargetId = 1
-                },
-                new MockWorkItemLinkInfo
-                {
-                    SourceId = 3,
-                    TargetId = 2
-                },
-                new MockWorkItemLinkInfo
-                {
-                    SourceId = 0,
-                    TargetId = 4
-                },
-                new MockWorkItemLinkInfo
-                {
-                    SourceId = 0,
-                    TargetId = 5
-                }
+                new MockWorkItemLinkInfo(0, 3),
+                new MockWorkItemLinkInfo(3, 1),
+                new MockWorkItemLinkInfo(3, 2),
+                new MockWorkItemLinkInfo(0, 4),
+                new MockWorkItemLinkInfo(0, 5)
             };
 
             WorkItemStore = new MockWorkItemStore(workItems, links);
