97d8ee4fda836a19efc80364076857b446dc99c0
Peter Lavallee <pelavall@microsoft.com>
2016-06-15 12:59:39 -0700

    Update ExceptionHandlingProxy to use CastleCore
    	- The previous implementation used RealProxy, which was not
    supported in a powershell environment. Additionally it made debugging
    difficult.
    	- Castle's implementation of a dynamic proxy fixes these issues

--- Qwiq/Qwiq.Core/Exceptions/ExceptionHandlingDynamicProxyFactory.cs
+++ Qwiq/Qwiq.Core/Exceptions/ExceptionHandlingDynamicProxyFactory.cs
@@ -1,14 +1,37 @@
-﻿namespace Microsoft.IE.Qwiq.Exceptions
+﻿using System.Collections.Generic;
+using Castle.DynamicProxy;
+
+namespace Microsoft.IE.Qwiq.Exceptions
 {
     public static class ExceptionHandlingDynamicProxyFactory
     {
+        private static readonly ProxyGenerator Generator = new ProxyGenerator();
+
         public static T Create<T>(T instance) where T : class
         {
-            return new ExceptionHandlingDynamicProxy<T>(
-                instance,
-                new ExceptionMapper(
-                    new IExceptionExploder[] {new AggregateExceptionExploder(), new InnerExceptionExploder()},
-                    new IExceptionMapper[] {new InvalidOperationExceptionMapper(), new TransientExceptionMapper()})).GetTransparentProxy() as T;
+            return Create(
+                    instance,
+                    new IExceptionExploder[]
+                    {
+                        new AggregateExceptionExploder(),
+                        new InnerExceptionExploder()
+                    },
+                    new IExceptionMapper[]
+                    {
+                        new InvalidOperationExceptionMapper(),
+                        new TransientExceptionMapper()
+                    });
+        }
+
+        internal static T Create<T>(T instance, IEnumerable<IExceptionExploder> exploders, IEnumerable<IExceptionMapper> mappers) where T : class
+        {
+            var proxy =
+                new ExceptionHandlingDynamicProxy<T>(
+                    new ExceptionMapper(
+                        exploders,
+                        mappers));
+
+            return (T)Generator.CreateInterfaceProxyWithTarget(typeof(T), instance, proxy);
         }
     }
 }
