e95ba38fed9ce74ce8dd8a69bc95726afaa1d309
Peter Lavallee <pelavall@microsoft.com>
2016-06-23 14:59:30 -0700

    Update Fragments to take ICollection not IEnumerable
    	-Replacing IEnumerable with ICollection will ensure when calling
    	evaluations down the line will not cause re-evaluation of the
    	data source

--- Qwiq/Qwiq.Linq/Fragments/ListFragment.cs
+++ Qwiq/Qwiq.Linq/Fragments/ListFragment.cs
@@ -6,16 +6,16 @@ namespace Microsoft.IE.Qwiq.Linq.Fragments
 {
     internal class ListFragment : IFragment
     {
-        private readonly IEnumerable<IFragment> _fragments;
+        private readonly ICollection<IFragment> _fragments;
 
-        public ListFragment(IEnumerable<IFragment> fragments)
+        public ListFragment(ICollection<IFragment> fragments)
         {
             _fragments = fragments;
         }
 
         public string Get(Type queryType)
         {
-            return "(" + String.Join(", ", _fragments.Select(s => s.Get(queryType))) + ")";
+            return "(" + string.Join(", ", _fragments.Select(s => s.Get(queryType))) + ")";
         }
 
         public bool IsValid()
