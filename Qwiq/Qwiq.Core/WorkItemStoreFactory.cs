2773ce6f6dd91c499a066e8168174f137624fec3
Richard Murillo <rjmurillo@gmail.com>
2016-08-26 14:39:34 -0700

    Update NuGet packages

--- Qwiq/Qwiq.Core/WorkItemStoreFactory.cs
+++ Qwiq/Qwiq.Core/WorkItemStoreFactory.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using Microsoft.IE.Qwiq.Proxies;
 using Microsoft.IE.Qwiq.Credentials;
 using Microsoft.IE.Qwiq.Exceptions;
-using Tfs = Microsoft.TeamFoundation;
+using Microsoft.IE.Qwiq.Proxies;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
 
 namespace Microsoft.IE.Qwiq
 {
@@ -13,29 +14,40 @@ namespace Microsoft.IE.Qwiq
 
     public class WorkItemStoreFactory : IWorkItemStoreFactory
     {
+        private static readonly Lazy<WorkItemStoreFactory> Instance = new Lazy<WorkItemStoreFactory>(() => new WorkItemStoreFactory());
+
         private WorkItemStoreFactory()
         {
         }
 
         public static IWorkItemStoreFactory GetInstance()
         {
-            return new WorkItemStoreFactory();
+            return Instance.Value;
         }
 
         public IWorkItemStore Create(Uri endpoint, TfsCredentials credentials)
         {
-            Func<Tfs.WorkItemTracking.Client.WorkItemStore, IQueryFactory> queryFactoryFunc = QueryFactory.GetInstance;
-            return Create(endpoint,credentials, queryFactoryFunc);
+            Func<WorkItemStore, IQueryFactory> queryFactoryFunc = QueryFactory.GetInstance;
+            return Create(endpoint, credentials, queryFactoryFunc);
         }
 
         internal IWorkItemStore Create(Uri endpoint, TfsCredentials credentials,
-            Func<Tfs.WorkItemTracking.Client.WorkItemStore, IQueryFactory> queryFactoryFunc)
+            Func<WorkItemStore, IQueryFactory> queryFactoryFunc)
         {
-            var tfs = ExceptionHandlingDynamicProxyFactory.Create<IInternalTfsTeamProjectCollection>(new TfsTeamProjectCollectionProxy(new Tfs.Client.TfsTeamProjectCollection(endpoint, credentials.Credentials)));
-            var workItemStore = tfs.GetService<Tfs.WorkItemTracking.Client.WorkItemStore>();
+            var tfs = ExceptionHandlingDynamicProxyFactory.Create<IInternalTfsTeamProjectCollection>(new TfsTeamProjectCollectionProxy(ConnectToTfsCollection(endpoint, credentials.Credentials)));
+            var workItemStore = tfs.GetService<WorkItemStore>();
             var queryFactory = queryFactoryFunc.Invoke(workItemStore);
 
             return ExceptionHandlingDynamicProxyFactory.Create<IWorkItemStore>(new WorkItemStoreProxy(tfs, workItemStore, queryFactory));
+        }
+
+        private static TfsTeamProjectCollection ConnectToTfsCollection(
+            Uri endpoint,
+            TfsClientCredentials credentials)
+        {
+            var tfsServer = new TfsTeamProjectCollection(endpoint, credentials);
+            tfsServer.EnsureAuthenticated();
+            return tfsServer;
         }
     }
 }
