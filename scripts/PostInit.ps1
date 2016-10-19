5546bfc11cc78a50025638e93ffb2e4b6e152146
Peter Lavallee <pelavall@microsoft.com>
2016-10-19 11:28:16 -0700

    Remove references and tools for VSO nuget packages

--- scripts/PostInit.ps1
+++
@@ -1,4 +0,0 @@
-& nuget install Microsoft.IE.IEPortal.Deploy -PreRelease -ExcludeVersion -OutputDirectory .\.tools
-& .\.tools\Microsoft.IE.IEPortal.Deploy\tools\init.ps1
-
-Copy-Item .\.tools\Microsoft.IE.IEPortal.Deploy\scripts\* .\scripts -Force
