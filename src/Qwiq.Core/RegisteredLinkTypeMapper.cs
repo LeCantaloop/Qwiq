9b226de9228efaee690d1ba692392affff650d18
Lyra1337 <tomthebig1337@gmail.com>
2017-02-16 16:45:11 +0100

    extended LinkMapper to support external links

---
+++ src/Qwiq.Core/RegisteredLinkTypeMapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Tfs = Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace Microsoft.Qwiq
+{
+    internal static class RegisteredLinkTypeMapper
+    {
+        public static Tfs.RegisteredLinkType Map(Tfs.WorkItemStore store, string linkTypeName)
+            => store.RegisteredLinkTypes
+                .OfType<Tfs.RegisteredLinkType>()
+                .FirstOrDefault(x => x.Name == linkTypeName);
+    }
+}
