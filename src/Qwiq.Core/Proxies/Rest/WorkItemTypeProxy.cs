0181bb3cadc8f53f9cd3b93b3bb2d0c6a780edfb
Richard Murillo <rimuri@microsoft.com>
2017-03-24 18:30:09 -0700

    Update exception for WorkItemTypeProxy.NewWorkItem
    This is not supported in the REST client. Comments also made (for posterity) regarding how a new work item would be created.

--- src/Qwiq.Core/Proxies/Rest/WorkItemTypeProxy.cs
+++ src/Qwiq.Core/Proxies/Rest/WorkItemTypeProxy.cs
@@ -6,8 +6,6 @@ namespace Microsoft.Qwiq.Proxies.Rest
 {
     public class WorkItemTypeProxy : IWorkItemType
     {
-        
-
         internal WorkItemTypeProxy(WorkItemType type)
         {
             Description = type.Description;
@@ -23,7 +21,29 @@ namespace Microsoft.Qwiq.Proxies.Rest
 
         public IWorkItem NewWorkItem()
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
+
+            /*
+             * In order to support this, we need:
+             *  1) an instance of WorkItemTrackingHttpClient (or WorkItemStoreProxy),
+             *  2) the name of the project (or IProject)
+             *
+             * Creating an item then looks like this
+             *
+             * var patch = new JsonPatchDocument();
+             * patch.Add(
+             *      new JsonPatchOperation()
+             *      {
+             *          Operation = Operation.Add,
+             *          Path = "/fields/System.Title",
+             *          Value = "Some Title"
+             *      }
+             * );
+             *
+             * var result = WorkItemStoreProxy.NativeClient.CreateWorkItemAsync(patch, projectName, Name).GetAwaiter().GetResult();
+             * return new WorkItemProxy(result);
+             *
+             */
         }
     }
 }
