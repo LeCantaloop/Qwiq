decb0be6165f9b79a927f9ca1224171883179f61
Richard Murillo <rimuri@microsoft.com>
2017-03-28 14:08:54 -0700

    Convert `MockFieldDefinitionCollection` to Core proxy

--- src/Qwiq.Core/Proxies/FieldDefinitionCollectionProxy.cs
+++ src/Qwiq.Core/Proxies/FieldDefinitionCollectionProxy.cs
@@ -4,22 +4,30 @@ using System.Linq;
 
 namespace Microsoft.Qwiq.Proxies
 {
-    internal abstract class FieldDefinitionCollectionProxy  : IFieldDefinitionCollection
+    public abstract class FieldDefinitionCollectionProxy : IFieldDefinitionCollection
     {
-        public abstract IEnumerator<IFieldDefinition> GetEnumerator();
+        public abstract int Count { get; }
 
+        public abstract IFieldDefinition this[string name] { get; }
+
+        public abstract bool Contains(string fieldName);
+
+        public abstract IEnumerator<IFieldDefinition> GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
 
-        public abstract int Count { get; }
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            if (ReferenceEquals(obj, null)) return false;
+            var fdc = obj as IFieldDefinitionCollection;
+            if (fdc == null) return false;
 
-        public abstract IFieldDefinition this[string name] { get; }
-
-
-        public abstract bool Contains(string fieldName);
+            return this.All(p => fdc.Contains(p, FieldDefinitionComparer.Instance));
+        }
 
         public override int GetHashCode()
         {
@@ -27,16 +35,6 @@ namespace Microsoft.Qwiq.Proxies
             {
                 return this.Aggregate(27, (current, f) => (13 * current) ^ f.GetHashCode());
             }
-        }
-
-        public override bool Equals(object obj)
-        {
-            if(ReferenceEquals(this, obj)) return true;
-            if (ReferenceEquals(obj, null)) return false;
-            var fdc = obj as IFieldDefinitionCollection;
-            if (fdc == null) return false;
-
-            return this.All(p => fdc.Contains(p, FieldDefinitionComparer.Instance));
         }
     }
 }
--- test/Qwiq.Mocks/MockFieldDefinitionCollection.cs
+++ test/Qwiq.Mocks/MockFieldDefinitionCollection.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,57 +6,50 @@ using Microsoft.Qwiq.Exceptions;
 
 namespace Microsoft.Qwiq.Mocks
 {
-    public class MockFieldDefinitionCollection : IFieldDefinitionCollection
+    public class MockFieldDefinitionCollection : Microsoft.Qwiq.Proxies.FieldDefinitionCollectionProxy
     {
         private readonly Dictionary<string, IFieldDefinition> _fieldUsagesByName;
 
         private readonly Dictionary<string, IFieldDefinition> _fieldUsagesByReferenceName;
 
-        public MockFieldDefinitionCollection(IEnumerable<IFieldDefinition> fieldDefintions)
+        public MockFieldDefinitionCollection(IEnumerable<IFieldDefinition> fieldDefinitions)
         {
-            if (fieldDefintions == null) throw new ArgumentNullException(nameof(fieldDefintions));
+            if (fieldDefinitions == null) throw new ArgumentNullException(nameof(fieldDefinitions));
 
-
-            _fieldUsagesByName = fieldDefintions.Where(p => !string.IsNullOrEmpty(p.Name)).ToDictionary(k => k.Name, e => e, StringComparer.OrdinalIgnoreCase);
-            _fieldUsagesByReferenceName = fieldDefintions.Where(p => !string.IsNullOrEmpty(p.ReferenceName)).ToDictionary(k => k.ReferenceName, e => e, StringComparer.OrdinalIgnoreCase);
+            _fieldUsagesByName = fieldDefinitions.Where(p => !string.IsNullOrWhiteSpace(p.Name)).ToDictionary(k => k.Name, e => e, StringComparer.OrdinalIgnoreCase);
+            _fieldUsagesByReferenceName = fieldDefinitions.Where(p => !string.IsNullOrWhiteSpace(p.ReferenceName)).ToDictionary(
+                k => k.ReferenceName,
+                e => e,
+                StringComparer.OrdinalIgnoreCase);
         }
 
+        public override int Count => _fieldUsagesByName.Count;
 
-
-        public IEnumerator<IFieldDefinition> GetEnumerator()
-        {
-            return _fieldUsagesByName.Values.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public int Count => _fieldUsagesByName.Count;
-
-        public IFieldDefinition this[string name]
+        public override IFieldDefinition this[string name]
         {
             get
             {
-                if (string.IsNullOrEmpty(name)) throw new ArgumentException("Value cannot be null or empty.", nameof(name));
+                if (string.IsNullOrEmpty(name))
+                    throw new ArgumentException("Value cannot be null or empty.", nameof(name));
 
                 if (!_fieldUsagesByName.TryGetValue(name, out IFieldDefinition def))
-                {
                     if (!_fieldUsagesByReferenceName.TryGetValue(name, out def))
-                    {
                         throw new FieldDefinitionNotExistException(name);
-                    }
-                }
 
                 return def;
             }
         }
 
-        public bool Contains(string fieldName)
+        public override bool Contains(string fieldName)
         {
-            if (string.IsNullOrEmpty(fieldName)) throw new ArgumentException("Value cannot be null or empty.", nameof(fieldName));
+            if (string.IsNullOrEmpty(fieldName))
+                throw new ArgumentException("Value cannot be null or empty.", nameof(fieldName));
             return _fieldUsagesByName.ContainsKey(fieldName) || _fieldUsagesByReferenceName.ContainsKey(fieldName);
+        }
+
+        public override IEnumerator<IFieldDefinition> GetEnumerator()
+        {
+            return _fieldUsagesByName.Values.GetEnumerator();
         }
     }
 }
