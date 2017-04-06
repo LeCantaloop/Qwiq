89a113f13a432d474d9dc229a882508fbfde8a83
Richard Murillo <rimuri@microsoft.com>
2017-04-06 08:13:34 -0700

    Separate IRevision and Revision

--- src/Qwiq.Core/IRevision.cs
+++ src/Qwiq.Core/IRevision.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-
 namespace Microsoft.Qwiq
 {
     public interface IRevision : IWorkItemCore
@@ -18,58 +15,6 @@ namespace Microsoft.Qwiq
         new object this[string name] { get; }
 
       
-    }
-
-    internal class Revision : IRevision
-    {
-        private readonly Lazy<IFieldCollection> _fields;
-
-        private Dictionary<int, object> _values;
-
-        internal Revision(
-            IFieldDefinitionCollection definitions,
-            int revision,
-            Func<IRevision, IFieldDefinitionCollection, IFieldCollection> fieldFactory)
-        {
-            Rev = revision;
-            _values = new Dictionary<int, object>();
-            _fields = new Lazy<IFieldCollection>(() => fieldFactory(this, definitions));
-        }
-
-        internal Revision(
-            WorkItem workItem,
-            int revision)
-        {
-            WorkItem = workItem ?? throw new ArgumentNullException(nameof(workItem));
-            Rev = revision;
-            _fields = new Lazy<IFieldCollection>(() => WorkItem.Fields);
-        }
-
-        public virtual object this[string name]
-        {
-            get
-            {
-                if (name == null) throw new ArgumentNullException(nameof(name));
-                return Fields[name].Value;
-            }
-        }
-
-        public int? Rev { get; }
-
-        object IWorkItemCore.this[string name]
-        {
-            get => this[name];
-            set => throw new NotSupportedException();
-        }
-
-        private WorkItem WorkItem { get; }
-
-        public IFieldCollection Fields => _fields.Value;
-
-       
-        public int? Id => WorkItem?.Id;
-
-        public string Url => WorkItem?.Url;
     }
 }
 
