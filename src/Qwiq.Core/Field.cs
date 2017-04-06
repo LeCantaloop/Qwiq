3660cca0242a5050e2799011df3c97b67c7e1263
Richard Murillo <rimuri@microsoft.com>
2017-04-06 08:10:53 -0700

    Separate IField and Field

---
+++ src/Qwiq.Core/Field.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Microsoft.Qwiq
+{
+    internal class Field : IField, IReadOnlyField
+    {
+        private readonly IRevisionInternal _revision;
+
+        private readonly IFieldDefinition _fieldDefinition;
+
+        protected internal Field(IRevisionInternal revision, IFieldDefinition fieldDefinition)
+        {
+            _revision = revision ?? throw new ArgumentNullException(nameof(revision));
+            _fieldDefinition = fieldDefinition ?? throw new ArgumentNullException(nameof(fieldDefinition));
+        }
+
+        public virtual bool IsValid => ValidationState == ValidationState.Valid;
+
+        public virtual string Name => _fieldDefinition.Name;
+
+        public virtual string ReferenceName => _fieldDefinition.ReferenceName;
+
+        public virtual object OriginalValue
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public virtual ValidationState ValidationState => throw new NotImplementedException();
+
+        public virtual bool IsChangedByUser => throw new NotImplementedException();
+
+        public virtual object Value
+        {
+            get => _revision.GetCurrentFieldValue(_fieldDefinition);
+            set => _revision.SetFieldValue(_fieldDefinition, value);
+        }
+
+        public virtual int Id => _fieldDefinition.Id;
+
+        public virtual bool IsDirty => throw new NotImplementedException();
+
+        public virtual bool IsEditable => throw new NotImplementedException();
+
+        public virtual bool IsRequired => throw new NotImplementedException();
+    }
+}
--- src/Qwiq.Core/IField.cs
+++ src/Qwiq.Core/IField.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Microsoft.Qwiq
 {
+    /// <summary>
+    /// Legacy interface
+    /// </summary>
     public interface IField
     {
         int Id { get; }
@@ -17,46 +20,10 @@ namespace Microsoft.Qwiq
         object Value { get; set; }
     }
 
-    internal class Field : IField
+    public interface IReadOnlyField : IIdentifiable<int>
     {
-        private readonly IRevisionInternal _revision;
-
-        private readonly IFieldDefinition _fieldDefinition;
-
-        protected internal Field(IRevisionInternal revision, IFieldDefinition fieldDefinition)
-        {
-            _revision = revision ?? throw new ArgumentNullException(nameof(revision));
-            _fieldDefinition = fieldDefinition ?? throw new ArgumentNullException(nameof(fieldDefinition));
-        }
-
-        public virtual bool IsValid => ValidationState == ValidationState.Valid;
-
-        public virtual string Name => _fieldDefinition.Name;
-
-        public virtual string ReferenceName => _fieldDefinition.ReferenceName;
-
-        public virtual object OriginalValue
-        {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
-        }
-
-        public virtual ValidationState ValidationState => throw new NotImplementedException();
-
-        public virtual bool IsChangedByUser => throw new NotImplementedException();
-
-        public virtual object Value
-        {
-            get => _revision.GetCurrentFieldValue(_fieldDefinition);
-            set => _revision.SetFieldValue(_fieldDefinition, value);
-        }
-
-        public virtual int Id => _fieldDefinition.Id;
-
-        public virtual bool IsDirty => throw new NotImplementedException();
-
-        public virtual bool IsEditable => throw new NotImplementedException();
-
-        public virtual bool IsRequired => throw new NotImplementedException();
+        string Name { get; }
+        string ReferenceName { get; }
+        object Value { get; }
     }
 }
