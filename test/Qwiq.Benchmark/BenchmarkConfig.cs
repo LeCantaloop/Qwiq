41b64276cfd38cf9ded121f0159efd98836b93f3
Richard Murillo <rimuri@microsoft.com>
2017-04-20 14:52:53 -0700

    Update BenchmarkConfig.cs
    Remove LegacyJit since we are running 4.7 with Ryu in production

--- test/Qwiq.Benchmark/BenchmarkConfig.cs
+++ test/Qwiq.Benchmark/BenchmarkConfig.cs
@@ -3,6 +3,7 @@ using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnostics.Windows;
 using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Validators;
 
 namespace Microsoft.Qwiq.Benchmark
 {
@@ -11,15 +12,18 @@ namespace Microsoft.Qwiq.Benchmark
         public BenchmarkConfig()
         {
             Add(Job.Clr.With(Jit.RyuJit).With(Platform.X64).With(new GcMode { Server = true }));
-            Add(Job.Clr.With(Jit.LegacyJit).With(Platform.X64).With(new GcMode { Server = true }));
-
             Add(Job.Clr.With(Jit.RyuJit).With(Platform.X86).With(new GcMode { Server = true }));
-            Add(Job.Clr.With(Jit.LegacyJit).With(Platform.X86).With(new GcMode { Server = true }));
+            Add(Job.Clr.With(Jit.RyuJit).With(Platform.AnyCpu).With(new GcMode { Server = true }));
 
+            // GC and Memory Allocation
             Add(new BenchmarkDotNet.Diagnosers.MemoryDiagnoser());
             Add(new InliningDiagnoser());
 
+            // Checks whether any of the referenced assemblies is non-optimized
+            Add(JitOptimizationsValidator.FailOnError);
+
             Add(StatisticColumn.AllStatistics);
+
 
         }
     }
