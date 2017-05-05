972f92f98e98138ffef2b8aae6368ee2ce41e0df
Richard Murillo <richard.murillo@microsoft.com>
2017-05-05 15:02:18 -0700

    Add methods to set field values on MockFieldCollection
    Useful for arranging conditions where fields are dirty or invalid

--- test/Qwiq.Mocks/MockFieldCollection.cs
+++ test/Qwiq.Mocks/MockFieldCollection.cs
@@ -1,10 +1,24 @@
+using JetBrains.Annotations;
+
 namespace Microsoft.Qwiq.Mocks
 {
     public class MockFieldCollection : FieldCollection
     {
-        public MockFieldCollection(WorkItemCore w, IFieldDefinitionCollection definitions)
+        public MockFieldCollection([NotNull] WorkItemCore w, [NotNull] IFieldDefinitionCollection definitions)
             : base(w, definitions, (r, d) => new MockField(r, d))
         {
+        }
+
+        public new void SetField([NotNull] IField field)
+        {
+            base.SetField(field);
+        }
+
+        public void SetFieldValue([NotNull] string name, [CanBeNull] object value)
+        {
+            TryGetByName(name, out IField f);
+            f.Value = value;
+            SetField(f);
         }
     }
 }
