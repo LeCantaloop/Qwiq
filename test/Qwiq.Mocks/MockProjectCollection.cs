be642cef2aa7298971734c169ecd681a1d332a54
Richard Murillo <richard.murillo@microsoft.com>
2017-05-05 14:58:49 -0700

    Add JetBrains annotations

--- test/Qwiq.Mocks/MockProjectCollection.cs
+++ test/Qwiq.Mocks/MockProjectCollection.cs
@@ -1,41 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+
+using JetBrains.Annotations;
 
 namespace Microsoft.Qwiq.Mocks
 {
     internal class MockProjectCollection : ProjectCollection
     {
-        public MockProjectCollection(IWorkItemStore store)
+        public MockProjectCollection([NotNull] IWorkItemStore store)
             : this(new MockProject(store))
         {
         }
 
-        public MockProjectCollection(MockProject project)
+        public MockProjectCollection([NotNull] MockProject project)
             : this(new[] { (IProject)project }.ToList())
         {
         }
 
-        public MockProjectCollection(List<IProject> projects)
+        public MockProjectCollection([NotNull] List<IProject> projects)
             : base(projects)
         {
-        }
-    }
-
-    internal class MockWorkItemTypeCollection : WorkItemTypeCollection
-    {
-        public MockWorkItemTypeCollection(IWorkItemStore store)
-            : base((List<IWorkItemType>)null)
-        {
-            ItemFactory = () => new[]
-                                    {
-                                        new MockWorkItemType(WorkItemTypeDefinitions.Task, null, store),
-                                        new MockWorkItemType(WorkItemTypeDefinitions.Deliverable, null, store),
-                                        new MockWorkItemType(WorkItemTypeDefinitions.Scenario, null, store),
-                                        new MockWorkItemType(WorkItemTypeDefinitions.CustomerPromise, null, store),
-                                        new MockWorkItemType(WorkItemTypeDefinitions.Bug, null, store),
-                                        new MockWorkItemType(WorkItemTypeDefinitions.Measure, null, store)
-                                    };
         }
     }
 }
