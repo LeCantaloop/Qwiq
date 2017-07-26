1f84f6c12105ff591724464d8fdb2aced22e7d45
Richard Murillo <rimuri@microsoft.com>
2017-07-26 12:59:02 -0700

    Remove unused using statement

--- src/Qwiq.Mapper/Attributes/AttributeMapperStrategy.cs
+++ src/Qwiq.Mapper/Attributes/AttributeMapperStrategy.cs
@@ -3,7 +3,6 @@ using JetBrains.Annotations;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 
