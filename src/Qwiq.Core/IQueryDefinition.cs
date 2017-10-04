68ea8102df26b033cc2d15f124f2c301ce8413ba
Richard Murillo <rimuri@microsoft.com>
2017-10-04 11:46:51 -0700

    Add annotations for properties

--- src/Qwiq.Core/IQueryDefinition.cs
+++ src/Qwiq.Core/IQueryDefinition.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 
+using JetBrains.Annotations;
+
 namespace Qwiq
 {
     public interface IQueryDefinition : IIdentifiable<Guid>, IEquatable<IQueryDefinition>
     {
+        [NotNull]
         string Name { get; }
+        [NotNull]
         string Wiql { get; }
     }
 }
