5d90dd6741e5dbecd8e03639a13bd5ec1586d188
Peter Lavallee <pelavall@microsoft.com>
2017-10-04 12:26:41 -0700

    Make exception handling in folder expansion more specific

--- src/Qwiq.Core.Rest/Project.cs
+++ src/Qwiq.Core.Rest/Project.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using JetBrains.Annotations;
-
 using Microsoft.TeamFoundation.Core.WebApi;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
 using Microsoft.VisualStudio.Services.WebApi;
@@ -11,6 +11,9 @@ namespace Qwiq.Client.Rest
 {
     internal class Project : Qwiq.Project
     {
+        //BUGBUG: There is a bug in the GetQueryAsync in vsts where if a folder contains a '+' character it will return 404, even if the folder exists see here: https://developercommunity.visualstudio.com/content/problem/123660/when-a-saved-query-folder-contains-a-character-it.html
+        private const string BadPathCharacter = "+";
+
         //The VSTS Rest api for shared queries only allows for expanding folder structure 2 deep, https://www.visualstudio.com/en-us/docs/integrate/api/wit/queries
         private const int MaxQueryFolderExpansionDepth = 2;
 
@@ -43,19 +46,37 @@ namespace Qwiq.Client.Rest
                 {
                     return new QueryFolderCollection(() =>
                     {
-                        //BUGBUG: There is a bug in the GetQueryAsync in vsts where if a folder contains a '+' character it will return 404, even if the folder exists see here: https://developercommunity.visualstudio.com/content/problem/123660/when-a-saved-query-folder-contains-a-character-it.html
                         QueryHierarchyItem FolderExpansionFunc(QueryHierarchyItem item)
                         {
                             try
                             {
-                                return store.NativeWorkItemStore.Value.GetQueryAsync(project.Id, item.Path, QueryExpand.Wiql, MaxQueryFolderExpansionDepth).Result;
+                                return
+                                    store
+                                    .NativeWorkItemStore
+                                    .Value
+                                    .GetQueryAsync(project.Id, item.Path, QueryExpand.Wiql, MaxQueryFolderExpansionDepth)
+                                    .GetAwaiter()
+                                    .GetResult();
                             }
-                            catch(VssServiceResponseException ex){
-                                throw new InvalidOperationException($"An error occured while trying to expand the saved query folder {item.Path}. If there is '+' in your folder path see, https://developercommunity.visualstudio.com/content/problem/123660/when-a-saved-query-folder-contains-a-character-it.html, otherwise see inner exception for details.", ex);
+                            catch(VssServiceResponseException ex)
+                            {
+                                if (ex.HttpStatusCode == HttpStatusCode.NotFound && item.Path.Contains(BadPathCharacter))
+                                {
+                                    throw new InvalidOperationException($"Unable to expand the saved query folder {item.Path} due to a bug in the Vso Query Service. Unable to retrieve folders with '{BadPathCharacter}' in the name. See bug, https://developercommunity.visualstudio.com/content/problem/123660/when-a-saved-query-folder-contains-a-character-it.html.", ex);
+                                }
+
+                                throw;
                             }
                         }
 
-                        var initialFolders = store.NativeWorkItemStore.Value.GetQueriesAsync(project.Id, QueryExpand.Wiql, MaxQueryFolderExpansionDepth).Result;
+                        var initialFolders =
+                                store
+                                .NativeWorkItemStore
+                                .Value
+                                .GetQueriesAsync(project.Id, QueryExpand.Wiql, MaxQueryFolderExpansionDepth)
+                                .GetAwaiter()
+                                .GetResult();
+
                         return initialFolders.Where(qf => qf != null).Select(qf => new QueryFolder(qf, FolderExpansionFunc));
                     });
                 })
