9abbcfda40c82959a7df25c171459d4b35d7dc30
Richard Murillo <rimuri@microsoft.com>
2018-01-19 13:55:53 -0800

    Add `SerializableAttribute` to `ServerRejectedChangesException`

--- src/Qwiq.Core/ServerRejectedChangesException.cs
+++ src/Qwiq.Core/ServerRejectedChangesException.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace Microsoft.Qwiq
 {
+    [Serializable]
     public class ServerRejectedChangesException : InvalidOperationException
     {
         internal ServerRejectedChangesException(Exception ex) : base(ex.Message, ex)
