d39d2f197583711f0747289f8473cc86584f4c26
Richard Murillo <rimuri@microsoft.com>
2018-01-19 12:28:28 -0800

    Remove shadow property `IFieldDefinition.Name`
    This was shadowed to enforce a ReSharper hint indicating that on a field definition the Name property cannot be null. ReSharper warnings in code paths for  `IFieldDefinition` indicate the value could be null (when it cannot be); however, this condition is guarded in multiple ways on the concrete class `FieldDefinition` in Core, which would prevent any such operation in practice.

--- src/Qwiq.Core/IFieldDefinition.cs
+++ src/Qwiq.Core/IFieldDefinition.cs
@@ -5,9 +5,6 @@ namespace Microsoft.Qwiq
     public interface IFieldDefinition : IIdentifiable<int>, INamed
     {
         [NotNull]
-        new string Name { get; }
-
-        [NotNull]
         string ReferenceName { get; }
     }
 }
